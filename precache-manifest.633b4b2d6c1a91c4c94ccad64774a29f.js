a57d0df387c0770fb418f0452f8a51706484bb48
Faisal Akhlaq <faisal.akhlaq@aol.com>
2021-04-18 00:38:10 +0200

    Updates

---
+++ precache-manifest.633b4b2d6c1a91c4c94ccad64774a29f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d1214e592c8909a12a9",
+    "url": "/anja-elfvig/static/js/runtime~main.5d1214e5.js"
+  },
+  {
+    "revision": "1764b0f310d4740ee807",
+    "url": "/anja-elfvig/static/js/main.1764b0f3.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/anja-elfvig/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "1764b0f310d4740ee807",
+    "url": "/anja-elfvig/static/css/main.1c72d1a3.chunk.css"
+  },
+  {
+    "revision": "d6387adb8ec98327ba7da440794685f4",
+    "url": "/anja-elfvig/index.html"
+  }
+];
