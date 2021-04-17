1a263b783e59e8f429b2fc35c7c7d9996a10dbf8
Faisal Akhlaq <faisal.akhlaq@aol.com>
2021-04-18 01:07:29 +0200

    first commit

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -22,7 +22,7 @@ export default class Introduction extends Component {
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/blog3.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -37,7 +37,7 @@ export default class Introduction extends Component {
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/blog4.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
